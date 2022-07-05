7f3656907b72454308bc669423ed9422537dab52
Cindy Dorantes <cindym.dorantes@gmail.com>
2022-07-04 21:20:07 -0500

    add new book

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
-function capture() {
+let newBook;
+
+// dataBase is the collection for all the books info
+const dataBase = [];
+
+// this function adds new input information to the dataBase and the HTML table
+function add() {
+  dataBase.push(newBook);
+  console.log(dataBase);
+
+  const table = document.getElementById('tbody'); 
+  const row = document.createElement('tr');
+  table.appendChild(row);
+  const dataTitle = document.createElement('td');
+  dataTitle.textContent = newBook.title;
+  row.appendChild(dataTitle);
+  const dataAuthor = document.createElement('td');
+  dataAuthor.textContent = newBook.author;
+  row.appendChild(dataAuthor);
+  const dataRemove = document.createElement('td');
+  row.appendChild(dataRemove);
+  const btnRemove = document.createElement('button');
+  btnRemove.textContent = 'Remove';
+  dataRemove.appendChild(btnRemove);
+}
+
+const btnAdd = document.getElementById('button');
+
+btnAdd.addEventListener('click', () => {
   function Store(title, author) {
     this.title = title;
     this.author = author;
@@ -8,12 +36,6 @@ function capture() {
   const nameAuthor = document.getElementById('author').value;
 
   newBook = new Store(nameTitle, nameAuthor);
-  console.log(newBook);
+  // console.log(newBook);
   add();
-}
-
-const dataBase = [];
-function add() {
-  dataBase.push(newBook);
-  document.getElementById('table').innerHTML += '<tbody><td>'+newBook.nameTitle+'</td><td>'+newBook.nameAuthor+'</td><td><button type="button" class="remove">Remove</button></td></tbody>';
-};
+});
